85c900d614f1cbde9e62483fff015f03aec5ffe0
FriedPhysicist <hiple1938@gmail.com>
2021-02-07 13:26:08 +0300

    problems

--- problems.cpp
+++ problems.cpp
@@ -22,12 +22,18 @@ void weird_algorithm()
 
 void missing_number()
 { 
-  int array_[4]={2,3,1,5};
+  int sum=0;
+  int total_sum=0;
+  int array_[4]={2,4,1,5};
 
   for(int i=0;i<size_array(array_);i++)
   {
-    cout<<array_[i]<< endl;
+    sum+=array_[i];
   }
+
+  for(int i=1;i<=size_array(array_)+1;i++) total_sum+=i;
+
+  cout<<total_sum-sum;
 }
 
 
