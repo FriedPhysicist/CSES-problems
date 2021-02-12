afd36f953fcdd2162fe8808ca07b5561775cde02
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-02-12 15:27:39 +0300

    3. problem solved

--- problems.cpp
+++ problems.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -15,7 +16,6 @@ void weird_algorithm()
 
   while(input>1)
   {
-	  //w
     input= input%2==0 ? input/2:input*3+1;
     cout << input << endl;
   }
@@ -34,12 +34,47 @@ void missing_number()
 
   for(int i=1;i<=size_array(array_)+1;i++) total_sum+=i;
 
-  cout<<total_sum-sum;
+  //cout<<total_sum-sum;
 }
 
+void repetitions()
+{
+  //A=1;T=2;C=3;G=3
+  string dna[]={"A","T","T","C","G","G","G","A"};
+  int dna_size=sizeof(dna)/sizeof(dna[0]);
+  int longest[]={0,0,0,0};
+  int most=0;
+  
+  for(int i=0;i<dna_size;i++)
+  {
+    if(dna[i]=="A"){
+      longest[0]++; 
+    }
 
-//w
+    if(dna[i]=="T"){
+      longest[1]++; 
+    }
+    
+    if(dna[i]=="C"){
+      longest[2]++;
+    }
+
+    if(dna[i]=="G"){
+      longest[3]++;
+    } 
+  }
+
+  for(int i=0;i<size_array(longest);i++)
+  {
+    if(longest[i]>most) {
+      most=longest[i];
+    }
+  } 
+
+  cout<<most<<endl;
+}
+
 int main()
 { 
-  missing_number();
+  repetitions();
 }
