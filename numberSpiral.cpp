db62e40a0aa8b53e4cf6f756d20a87e56a40e906
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-05-15 21:41:26 +0300

    sprial

--- numberSpiral.cpp
+++ numberSpiral.cpp
@@ -2,8 +2,35 @@
 
 using namespace std;
 
+int column;
+int row;
+
+int middleNumber(int number)
+{
+    return number*number - (number - 1);
+}
+
 int main()
 {
+    cout<< "column: " << endl; 
+    cin >> column;  
+
+    cout<< "row: " << endl;
+    cin >> row;  
+
+    //if number on column bigger
+    if(column > row)
+    {
+    }
+
+    //if number on row bigger
+    else if(column < row)
+    {
+    }
+    
+    else{
+	cout << middleNumber(column) << endl
+    }
 }
 
 
--- permutations.cpp
+++ permutations.cpp
@@ -9,6 +9,12 @@ int main()
     cin >>n;
     cout<<endl;
 
+    if(n<=4)
+    {
+	cout<<"no combination"<<endl;
+	return 0;
+    }
+
     for (int i = 1; i <= n; i += 2) cout << i << " ";
     for (int i = 2; i <= n; i += 2) cout << i << " ";
     
